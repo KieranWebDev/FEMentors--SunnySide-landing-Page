031ed301d59737a326b99f8342618a26b31cd84a
Kieran <kieran.webdevelopment@gmail.com>
2023-01-20 15:55:32 +0000

    added js to open links in new tab on desktop but not on mobile

--- javascript.js
+++ javascript.js
@@ -4,3 +4,21 @@ const menu = document.querySelector('.menu');
 menuBtn.addEventListener('click', () => {
   menu.classList.toggle('show-menu');
 });
+
+// anchor links open in new tab on desktop view only (not mobile)
+const links = document.querySelectorAll('a');
+// const mq = window.matchMedia('(min-width: 768px)');
+
+// if (mq.matches) {
+//   links.forEach((link) => {
+//     link.setAttribute('target', '_blank');
+//   });
+// }
+
+links.forEach((link) => {
+  if (window.innerWidth <= 550) {
+    link.setAttribute('target', '_self');
+  } else {
+    link.setAttribute('target', '_blank');
+  }
+});
