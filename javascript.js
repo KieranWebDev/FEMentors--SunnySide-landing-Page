91aef751c545fc4543bcd5d8ee3c35bd85f450db
Kieran <kieran.webdevelopment@gmail.com>
2023-01-31 13:50:07 +0000

    cleaned  up clutter in codebase

--- javascript.js
+++ javascript.js
@@ -1,19 +1,14 @@
+// selectors
 const menuBtn = document.querySelector('.hamburger-btn');
 const menu = document.querySelector('.menu');
 
+// hamburber menu functinality
 menuBtn.addEventListener('click', () => {
   menu.classList.toggle('show-menu');
 });
 
-// anchor links open in new tab on desktop view only (not mobile)
+// anchor links open in new tab on desktop and tablet view only (not mobile)
 const links = document.querySelectorAll('a');
-// const mq = window.matchMedia('(min-width: 768px)');
-
-// if (mq.matches) {
-//   links.forEach((link) => {
-//     link.setAttribute('target', '_blank');
-//   });
-// }
 
 links.forEach((link) => {
   if (window.innerWidth <= 550) {
